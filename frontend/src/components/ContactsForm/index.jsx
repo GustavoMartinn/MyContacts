ef0107ccd72c1686b063668817b218a22a8ea791
GustavoMartinn <gumartin29@gmail.com>
2022-12-29 15:03:53 -0300

    refactor: change form to controlled

--- frontend/src/components/ContactsForm/index.jsx
+++ frontend/src/components/ContactsForm/index.jsx
@@ -5,22 +5,47 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 import { ButtonContainer, Form } from './styles';
+import { useState } from 'react';
 
 export default function ContactsForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [socialNetwork, setSocialNetwork] = useState('');
+
   return (
     <Form>
-      <FormGroup error="O formato é invalido">
-        <Input placeholder='Nome' error />
+      <FormGroup>
+        <Input
+          placeholder='Nome'
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Input placeholder='E-mail' />
+        <Input
+          placeholder='E-mail'
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Input placeholder='Telefone' />
+        <Input
+          placeholder='Telefone'
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Select>
+        <Select
+          value={socialNetwork}
+          onChange={(event) => setSocialNetwork(event.target.value)}
+        >
           <option value=''>Selecione uma rede social</option>
+          <option value='facebook'>Facebook</option>
+          <option value='instagram'>Instagram</option>
+          <option value='linkedin'>LinkedIn</option>
+          <option value='twitter'>Twitter</option>
         </Select>
       </FormGroup>
       <ButtonContainer>
